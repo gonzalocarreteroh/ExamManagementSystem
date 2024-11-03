8f280c8e7b1dd2506d495d03ca64c840451b84e5
Gonzalo Carretero <gcarrete@uwaterloo.ca>
2024-11-03 20:43:44 +0800

    Added test structure

---
+++ AppMain/src/test/java/comp3111/examsystem/MainTest.java
@@ -0,0 +1,14 @@
+package comp3111.examsystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void main() {
+        // Example test logic here
+        assertEquals(1, 1);
+    }
+}
