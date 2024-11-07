4a138f994359352c78582647e698d791781a0531
Richard Blažek <blazek.risa@gmail.com>
2024-11-08 05:36:38 +0800

    ExamTest (notice the irony)

--- AppMain/src/main/java/comp3111/examsystem/model/Exam.java
+++ AppMain/src/main/java/comp3111/examsystem/model/Exam.java
@@ -1,18 +1,24 @@
 package comp3111.examsystem.model;
 
 public class Exam {
+    private final int id;
     private final String name;
     private final int duration;
     private final int courseId;
     private final boolean published;
     private final int[] questionIds;
 
-    public Exam(String name, int duration, int courseId, boolean published, int[] questionIds) {
+    public Exam(int id, String name, int duration, int courseId, boolean published, int[] questionIds) {
+        this.id = id;
         this.name = name;
         this.duration = duration;
         this.courseId = courseId;
         this.published = published;
         this.questionIds = questionIds;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getName() {
---
+++ AppMain/src/test/java/comp3111/examsystem/model/ExamTest.java
@@ -0,0 +1,29 @@
+package comp3111.examsystem.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class ExamTest {
+    @Test
+    void gettersTest() {
+        CourseDb db = new CourseDb();
+        db.add("COMP3111", "SwEng", "CS");
+        int courseId = db.all()[0].getId();
+
+        QuestionDb qb = new QuestionDb();
+        qb.add("1+1=", "1", "2", "11", "10", "B", 4);
+        qb.add("1+1=", "1", "2", "11", "10", "B", 4);
+        int qId1 = qb.all()[0].getId(), qId2 = qb.all()[1].getId();
+
+        Exam x = new Exam(3, "Midterm", 60, courseId, false, new int[]{qId1, qId2});
+        assertEquals(3, x.getId());
+        assertEquals("Midterm", x.getName());
+        assertEquals(60, x.getDuration());
+        assertEquals(courseId, x.getCourse(db).getId());
+        assertFalse(x.getPublished());
+        assertEquals(qId1, x.getQuestions(qb)[0].getId());
+        assertEquals(qId2, x.getQuestions(qb)[1].getId());
+    }
+}
